916fd749957e08c2f0f239f5270d32d141be9208
Patrick Grogan <pgrogan@drw.com>
2020-12-03 21:12:03 -0500

    Change syntax highlight that is list based

--- systemverilog-mode.el
+++ systemverilog-mode.el
@@ -29,25 +29,6 @@
     "primitive" "release" "repeat" "specify" "specparam" "table" "task" "wait" "while"
     )
   "List of all keywords from Verilog 95 specification.")
-(defconst verilog-95-types
-  '(
-    "highz0" "inout" "input" "integer" "large" "medium" "output" "real" "realtime"
-    "reg" "scalared" "small" "supply0" "supply1" "time" "tri" "tri0" "tri1" "triand"
-    "trior" "trireg" "vectored" "wand" "wire" "wor"
-    )
-  "List of all types from Verilog 95 specification.")
-(defconst verilog-95-builtin-primitives
-  '(
-    "and" "buf" "bufif0" "bufif1" "cmos" "nand" "nmos" "nor" "not" "notif0" "notif1"
-    "or" "pmos" "rcmos" "rnmos" "rpmos" "rtran" "rtranif0" "rtranif1" "tran" "tranif0"
-    "tranif1" "xnor" "xor"
-    )
-  "List of all builtin primitives from Verilog 95 specification.")
-(defconst verilog-95-strength
-  '(
-    "highz1" "pull0" "pull1" "pulldown" "pullup" "strong0" "strong1" "weak0" "weak1"
-    )
-  "List of all strength words from Verilog 95 specification.")
 (defconst verilog-01-keywords
   '(
     "automatic" "incdir" "include" "cell" "pulsestyle_ondetect" "pulsestyle_onevent"
@@ -55,12 +36,7 @@
     "localparam" "use" "noshowcancelled"
     )
   "List of all keywords from Verilog 2001 specification.")
-(defconst verilog-01-types
-  '(
-"signed" "unsigned" "genvar"
-    )
-  "List of all types from Verilog 2001 specification.")
-(defconst systemverilog-09-keywords
+(defconst systemverilog-05-keywords
   '(
     "accept_on" "alias" "always_comb" "always_ff" "always_latch" "assert" "assume"
     "before" "bind" "binsof" "break" "chandle" "checker" "class" "clocking" "const"
@@ -78,13 +54,37 @@
     "void" "wait_order" "wildcard" "with" "within"
     )
   "List of all keywords from Systemverilog specification.")
-(defconst systemverilog-09-types
+(defconst verilog-95-types
+  '(
+    "highz0" "inout" "input" "integer" "large" "medium" "output" "real" "realtime"
+    "reg" "scalared" "small" "supply0" "supply1" "time" "tri" "tri0" "tri1" "triand"
+    "trior" "trireg" "vectored" "wand" "wire" "wor"
+    )
+  "List of all types from Verilog 95 specification.")
+(defconst verilog-01-types
+  '(
+"signed" "unsigned" "genvar"
+    )
+  "List of all types from Verilog 2001 specification.")
+(defconst systemverilog-05-types
   '(
     "bins" "bit" "byte" "coverpoint" "int" "logic" "longint" "rand" "randc"
     "randsequence" "sequence" "shortint" "shortreal" "string" "var"
     )
   "List of all types from Systemverilog specification.")
-(defconst systemverilog-09-strength
+(defconst verilog-95-builtin-primitives
+  '(
+    "and" "buf" "bufif0" "bufif1" "cmos" "nand" "nmos" "nor" "not" "notif0" "notif1"
+    "or" "pmos" "rcmos" "rnmos" "rpmos" "rtran" "rtranif0" "rtranif1" "tran" "tranif0"
+    "tranif1" "xnor" "xor"
+    )
+  "List of all builtin primitives from Verilog 95 specification.")
+(defconst verilog-95-strength
+  '(
+    "highz1" "pull0" "pull1" "pulldown" "pullup" "strong0" "strong1" "weak0" "weak1"
+    )
+  "List of all strength words from Verilog 95 specification.")
+(defconst systemverilog-05-strength
   '(
     "strong" "weak"
     )
@@ -92,11 +92,35 @@
 
 (defun systemverilog-font-words-init ()
   "Create all reserved keywords/type list from the constants."
-  (setq systemverilog-keywords
-        (append verilog-95-keywords
-                verilog-01-keywords
-                systemverilog-09-keywords))
-  )
+  (setq systemverilog-mode-font-lock-keywords
+        (let* (
+               (x-keywords (append verilog-95-keywords
+                                   verilog-01-keywords
+                                   systemverilog-05-keywords))
+               (x-types (append verilog-95-types
+                                verilog-01-types
+                                systemverilog-05-types))
+               (x-builtins (append verilog-95-builtin-primitives))
+               (x-strength (append verilog-95-strength
+                                   systemverilog-05-strength))
+               (x-keywords-regexp (regexp-opt x-keywords 'words))
+               (x-types-regexp (regexp-opt x-types 'words))
+               (x-builtin-primitives-regexp (regexp-opt x-builtins 'words))
+               (x-strength-regexp (regexp-opt x-strength 'words)))
+          `(
+            (,x-keywords-regexp . font-lock-keyword-face)
+            (,x-types-regexp . font-lock-type-face)
+            (,x-builtin-primitives-regexp . font-lock-builtin-face)
+            (,x-strength-regexp . font-lock-type-face)
+            ;Preprocessor
+            ("`define\\|`ifdef\\|`else\\|`endif" . font-lock-preprocessor-face)
+            ; Synthesizer directives
+            ; TODO: Maybe we could redefine this per tool but for now lets be generic
+            ("(\\* .* \\*)" . font-lock-comment-face)
+            ; System calls, we could eventually make a list to only highlight systemcalls or not...)
+            ; System calls always start with $ and finish at a whitespace or a opening parenthesis
+            ("\\$.*\\( \\|(\\)" . font-lock-keyword-face))
+          )))
 
 ;;;###autoload
 (define-derived-mode systemverilog-mode prog-mode "systemverilog mode"
